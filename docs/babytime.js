f2ed39d1150574f7f3f816886cc6e05ca54f0b0b
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2022-12-26 12:29:01 -0500

    Saving via cookie

--- docs/babytime.js
+++ docs/babytime.js
@@ -1,23 +1,3 @@
 
-var BabyTimeUtils = {
-    cookieSave : function(state,prefix="baby_time") {
-        var txt = JSON.stringify(state);
-        document.cookie = prefix + "=" + txt;
-    },
-    cookieTryLoad : function(prefix="baby_time") {
-        var parts = document.cookie.split(";");
-        prefix = prefix + "=";
-        for (var i in parts) {
-            var p = parts[i].trim();
-            if (p.startsWith(prefix)) {
-                p = p.replace(prefix,"");
-                var obj = JSON.parse(p);
-                return obj;
-            }
-        }
-        return false;
-    },
-};
-
 
 
